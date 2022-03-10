eef7ae061493d4dc96663b7aee9d85579d01426d
GOPAL THAKUR <gopalk557@gmail.com>
2022-03-10 19:25:18 +0530

    [Gopal]Add:Welcome to the Mood analser Problems

---
+++ MoodAnalyserProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Mood Analyser programs");
+            Console.ReadLine();
+        }
+    }
+}
